1ef18c8db2c807f1d443222f86059c0a0855661c
Steviggio <steve.mothmora@gmail.com>
2026-05-23 14:54:07 +0200

    chore: remove .agent and test file from tracking

--- platform/test-reddit.ts
+++
@@ -1,9 +0,0 @@
-import { ScraperService } from './src/worker/scraper.service';
-
-async function test() {
-  const scraper = new ScraperService();
-  const res = await scraper.findRssOrLinks('https://www.reddit.com/r/technews/');
-  console.log(JSON.stringify(res, null, 2));
-}
-
-test();
